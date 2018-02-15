8ebd6a86c3bb0249e8418e55d79aca5e1db3cdb8
mimitch2 <mike@melodix.com>
2018-02-15 09:49:50 -0600

    second push

--- 04week/loop.js
+++ 04week/loop.js
@@ -7,12 +7,52 @@ const persons = {
   lasName: "Doe",
   birthDate: "Jan 5, 1925",
   gender: "female"
-}
+};
 
-const ForLoopFunction=()=> {
-  for (var i = 0; i < carsInReverse.length; i++) {
-    console.log(carsInReverse[i]);
+// const ForLoopFunction=()=> {
+//   for (var i = 0; i < carsInReverse.length; i++) {
+//     console.log(carsInReverse[i]);
+//   }
+// }
+//
+// ForLoopFunction();
+//
+//
+// const forInFunction=()=> {
+//   for (let x in persons) {
+//     console.log(x);
+//   }
+// }
+
+// forInFunction();
+
+const forInIfFunction=()=> {
+  for (x in persons) {
+    if (persons.hasOwnProperty(x)) {
+      console.log(x);
+    }
   }
 }
 
-ForLoopFunction();
+forInIfFunction();
+
+// const whileLoopFunction=()=> {
+//   let x = 0;
+//   while (x <= 1000) {
+//     console.log(x);
+//     x++;
+//   }
+// }
+//
+// whileLoopFunction();
+//
+//
+// const doWhileFunction=()=> {
+//   let x = 0;
+//   do {
+//     console.log(x);
+//     x++;
+//   } while (x <= 1000);
+// }
+//
+// doWhileFunction();
