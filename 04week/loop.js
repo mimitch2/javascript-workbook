79d62de5896fdb17bcc2e3cb80f1479d5441db94
mimitch2 <mike@melodix.com>
2018-02-15 11:20:39 -0600

    third loop push

--- 04week/loop.js
+++ 04week/loop.js
@@ -9,50 +9,51 @@ const persons = {
   gender: "female"
 };
 
-// const ForLoopFunction=()=> {
-//   for (var i = 0; i < carsInReverse.length; i++) {
-//     console.log(carsInReverse[i]);
-//   }
-// }
-//
-// ForLoopFunction();
-//
-//
-// const forInFunction=()=> {
-//   for (let x in persons) {
-//     console.log(x);
-//   }
-// }
+const ForLoopFunction=()=> {
+  for (var i = 0; i < carsInReverse.length; i++) {
+    console.log(carsInReverse[i]);
+  }
+}
 
-// forInFunction();
+ForLoopFunction();
+
+const forInFunction=()=> {
+  for (let x in persons) {
+    console.log(x);
+  }
+}
+
+forInFunction();
 
 const forInIfFunction=()=> {
-  for (x in persons) {
-    if (persons.hasOwnProperty(x)) {
-      console.log(x);
-    }
+  // for (let key in persons) {
+  if (persons.hasOwnProperty('birthDate')) {
+    console.log(persons['birthDate']);
+  }else{
+    console.log('no key');
   }
+  // }
 }
 
 forInIfFunction();
 
-// const whileLoopFunction=()=> {
-//   let x = 0;
-//   while (x <= 1000) {
-//     console.log(x);
-//     x++;
-//   }
-// }
-//
-// whileLoopFunction();
-//
-//
-// const doWhileFunction=()=> {
-//   let x = 0;
-//   do {
-//     console.log(x);
-//     x++;
-//   } while (x <= 1000);
-// }
-//
-// doWhileFunction();
+const whileLoopFunction=()=> {
+  let x = 0;
+  while (x <= 1000) {
+    console.log(x);
+    x++;
+  }
+}
+
+whileLoopFunction();
+
+
+const doWhileFunction=()=> {
+  let x = 0;
+  do {
+    console.log(x);
+    x++;
+  } while (x <= 1000);
+}
+
+doWhileFunction();
