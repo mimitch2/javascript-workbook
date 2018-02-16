15f7391b76c6f38f5874f703fe6e835726eef40c
mimitch2 <mike@melodix.com>
2018-02-16 12:40:02 -0600

    first space commit, all but 1 tests pass

--- 05week/spaceTravelToMars.js
+++ 05week/spaceTravelToMars.js
@@ -11,6 +11,66 @@ let jobTypes = {
 
 // Your code here
 
+// we need to create 2 classes, CrewMember and Ship
+// CrewMember will have 4 items, name, job, specialSkill and Ship
+// Ship will have name, type, ability and an empty array called crew for storing crew members
+// set conditions for who can enter a ship, using the jobType object to match with the ship's abilties
+// Make sure you only get the ship's mission statement IF the crew memebers job matches the ship's ability
+
+class CrewMember {
+  constructor (name, job, specialSkill, ship) {
+    this.name = name;
+    this.job = job;
+    this.specialSkill =  specialSkill;
+    this.ship = ship;
+  }
+  enterShip(shipName){
+    shipName.crew.push(this)
+    this.ship = shipName;
+    // console.log(shipName.crew[0].job);
+
+    // console.log('inside EnterShip');
+    // console.log(this.name);
+    // console.log(this.ship.name);
+    // console.log(shipName.crew);
+    // console.log(shipName.crew.length);
+    // console.log(typeof shipName.crew);
+  }
+}
+
+
+
+class Ship {
+  constructor (name, type, ability, crew) {
+    this.name = name;
+    this.type = type;
+    this.ability =  ability;
+    this.crew = [];
+
+  }
+  missionStatement(){
+    let jobLookup = jobTypes[this.crew[0].job]
+    console.log(this.crew[0].ship.type);
+    console.log(jobLookup);
+    // console.log(this.crew[0].job);
+    // console.log(this.type);
+    if (this.crew[0].ship.type === jobTypes[this.crew[0].job]){
+      return this.ability
+    }else{
+      return "Can't perform a mission yet."
+    }
+  }
+}
+//
+let crewMember1 = new CrewMember('Rick Martinez', 'pilot', 'chemistry');
+// // let crewMember2 = new CrewMember('Commander Lewis', 'commander', 'geology');
+let mav = new Ship('Mars Ascent Vehicle', 'MAV', 'Ascend into low orbit');
+// // let hermes = new Ship('Hermes', 'Main Ship', 'Interplanetary Space Travel');
+//
+crewMember1.enterShip(mav)
+// // // crewMember2.enterShip(hermes)
+mav.missionStatement();
+
 //tests
 if (typeof describe === 'function'){
   describe('CrewMember', function(){
