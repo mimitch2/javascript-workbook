e74f67c8a6c4ced556d52ff2b87b09286a64a7a8
mimitch2 <mike@melodix.com>
2018-02-22 09:16:20 -0600

    addressed teachers comments

--- 03week/towersOfHanoi.js
+++ 03week/towersOfHanoi.js
@@ -1,5 +1,5 @@
 'use strict';
-const colors = require('colors');//required for colors in terminal
+const colors = require('colors'); //required for colors in terminal
 const assert = require('assert');
 const readline = require('readline');
 const rl = readline.createInterface({
@@ -13,90 +13,74 @@ let stacks = {
   c: []
 };
 
-let moves = 0;//to track moves so we only have to check for win after 14
+let moves = 0; //to track moves so we only have to check for win after 14
 
-const printStacks=()=> {
-  console.log(`Moves: ${moves}`.cyan.underline);//log moves for every turn in cyan and underlined
+const printStacks = () => {
+  console.log(`Moves: ${moves}`.cyan.underline); //log moves for every turn in cyan and underlined
   console.log("a: " + stacks.a);
   console.log("b: " + stacks.b);
   console.log("c: " + stacks.c);
 }
 
-const isInputValid=(startStack, endStack)=> {//set tests that user input is valid
-  const validInputs = /[abc]/;//valid inputs
-  if (startStack.length === 1 && endStack.length === 1 &&
-      startStack.search(validInputs) === 0 && endStack.search(validInputs) === 0){
-        //checks inputs are only 1 character, and match a,b or c
-    return true
-  }else{//if false, call announceInvalid
-    announceInvalid();
-  }
+const isInputValid = (startStack, endStack) => { //set tests that user input is valid
+  const validInputs = /[abc]/; //valid inputs
+  return startStack.length === 1 && endStack.length === 1 && //checks inputs are only 1 character,
+    startStack.search(validInputs) === 0 && endStack.search(validInputs) === 0//and match a,b or c
 }
 
-const announceInvalid=(startStack, endStack)=>{//only if isInputValid fails
-  console.log(`\n\u{274C}  Invalid Input!! Make sure you enter either a, b or c.\n`.red);
+const isEmptyStart = (startStack, endStack) => {
+  return stacks[startStack].length !== 0 //checks that startStack is not empty
 }
 
-const isLegal=(startStack, endStack)=>{
-  if (isEmptyStart(startStack, endStack) && isBiggerThan(startStack, endStack)){//calls both functions to determine both are true
-    return true
-  }
+const isBiggerThan = (startStack, endStack) => {
+  return stacks[endStack].length === 0 || stacks[endStack].slice(-1) > stacks[startStack].slice(-1)
+  //if endStack is emptey OR if end's last array item is greater than start's last
 }
 
-const isEmptyStart=(startStack, endStack)=> {
-  if (stacks[startStack].length !== 0){//checks that startStack is not empty
-    return true
-  }else{//if it is empty throw error
-    console.log(`\n\u{274C}  You can't do that!!! You cannot move from an empty stack!\n`.red);
-  }
+const movePiece = (startStack, endStack) => {
+  stacks[endStack].push(stacks[startStack].pop()); //use pop and push to move between arrays
+  moves++; //iterate moves on each legal move
 }
 
-const isBiggerThan=(startStack, endStack)=> {
-  if (stacks[endStack].length === 0 || stacks[endStack].slice(-1) > stacks[startStack].slice(-1)){
-    //if endStack is emptey OR if end's last array item is greater than start's last
-    return true
-  }else{//if false throw error
-    console.log(`\n\u{274C}  You can't do that!!! endStack's last number must be greater that startStack's number.\n`.red);
-
-  }
+const checkForWin = (startStack, endStack) => {
+  return stacks.c.length === 4 //see if last stack= 4,3,2,1
 }
 
-const movePiece=(startStack, endStack)=> {
-  stacks[endStack].push(stacks[startStack].pop());//use pop and push to move between arrays
-  moves++;//iterate moves on each legal move
-}
-
-const checkForWin=(startStack, endStack)=> {
-  if (stacks.c.length === 4) {//see if last stack= 4,3,2,1
-    return true
-  }
-}
-
-const resetGame=()=> {//reset board and moves
+const resetGame = () => { //reset board and moves
   stacks = {
-    a: [4,3,2,1],
+    a: [4, 3, 2, 1],
     b: [],
     c: []
   };
   moves = 0;
 }
 
-const towersOfHanoi=(startStack, endStack)=> {
+const towersOfHanoi = (startStack, endStack) => {
   startStack = startStack.toLowerCase();
-  endStack = endStack.toLowerCase();//force inputs to lowercase
-  if (isInputValid(startStack, endStack) && isLegal(startStack, endStack)) {//check that input is valid, and move is legal, if so call movePiece
-    movePiece(startStack, endStack);
-    if (moves>13) {//only check for win starting at 14 moves since 15 is minimum to win
-      if (checkForWin(startStack, endStack)){
-        printStacks();//show the winning board first
-        console.log(`\n\u{1F3C6}  WINNER with only ${moves} tries! \n \u{2B07} NEW GAME`.green);
-        resetGame();//reset for new game
+  endStack = endStack.toLowerCase(); //force inputs to lowercase
+  if (isInputValid(startStack, endStack)) { //check that input is valid
+    if (isEmptyStart(startStack, endStack)) { //if emptyStart is true
+      if (isBiggerThan(startStack, endStack)) { //if isBiggertahn is true
+        movePiece(startStack, endStack); //move peice
+      } else {
+        console.log(`\n\u{274C}  You can't do that!!! endStack's last number must be greater that startStack's number.\n`.red);
       }
+    } else {
+      console.log(`\n\u{274C}  You can't do that!!! You cannot move from an empty stack!\n`.red);
+    }
+  } else {
+    console.log(`\n\u{274C}  Invalid Input!! Make sure you enter either a, b or c.\n`.red);
+  }
+  if (moves > 13) { //only check for win starting at 14 moves since 15 is minimum to win
+    if (checkForWin(startStack, endStack)) {
+      printStacks(); //show the winning board first
+      console.log(`\n\u{1F3C6}  WINNER with only ${moves} tries! \n \u{2B07} NEW GAME`.green);
+      resetGame(); //reset for new game
     }
   }
 }
 
-const getPrompt=()=> {
+const getPrompt = () => {
   printStacks();
   rl.question('start stack: ', (startStack) => {
     rl.question('end stack: ', (endStack) => {
@@ -118,41 +102,41 @@ if (typeof describe === 'function') {
   describe('#towersOfHanoi()', () => {
     it('prevent invalid inputs', () => {
       towersOfHanoi('34', '  L');
-      assert.deepEqual(stacks.a, [4,3,2,1]);
+      assert.deepEqual(stacks.a, [4, 3, 2, 1]);
       assert.deepEqual(stacks.b, []);
       assert.deepEqual(stacks.c, []);
     });
     it('change input to lowercase', () => {
       towersOfHanoi('A', 'B');
-      assert.deepEqual(stacks.a, [4,3,2]);
+      assert.deepEqual(stacks.a, [4, 3, 2]);
       assert.deepEqual(stacks.b, [1]);
       assert.deepEqual(stacks.c, []);
     });
     it('should move from one stack to another empty stack', () => {
       stacks = {
-        a: [4,3,2,1],
+        a: [4, 3, 2, 1],
         b: [],
         c: []
       };
       towersOfHanoi('a', 'b');
-      assert.deepEqual(stacks.a, [4,3,2]);
+      assert.deepEqual(stacks.a, [4, 3, 2]);
       assert.deepEqual(stacks.b, [1]);
     });
     it('should prevent moving to a stack with a larger number', () => {
       towersOfHanoi('a', 'b');
-      assert.deepEqual(stacks.a, [4,3,2]);
+      assert.deepEqual(stacks.a, [4, 3, 2]);
       assert.deepEqual(stacks.b, [1]);
       assert.deepEqual(stacks.c, []);
     });
     it('should prevent moving from an empty stack', () => {
       towersOfHanoi('c', 'b');
-      assert.deepEqual(stacks.a, [4,3,2]);
+      assert.deepEqual(stacks.a, [4, 3, 2]);
       assert.deepEqual(stacks.b, [1]);
       assert.deepEqual(stacks.c, []);
     });
     it('should allow moving to a stack with a larger number', () => {
       towersOfHanoi('b', 'a');
-      assert.deepEqual(stacks.a, [4,3,2,1]);
+      assert.deepEqual(stacks.a, [4, 3, 2, 1]);
       assert.deepEqual(stacks.b, []);
       assert.deepEqual(stacks.c, []);
     });
@@ -160,22 +144,22 @@ if (typeof describe === 'function') {
       stacks = {
         a: [],
         b: [1],
-        c: [4,3,2]
+        c: [4, 3, 2]
       };
       towersOfHanoi('b', 'c');
       assert.deepEqual(stacks.a, []);
       assert.deepEqual(stacks.b, []);
-      assert.deepEqual(stacks.c, [4,3,2,1]);
+      assert.deepEqual(stacks.c, [4, 3, 2, 1]);
     });
     it('should reset game', () => {
       stacks = {
         a: [2],
         b: [1],
-        c: [4,3]
+        c: [4, 3]
       };
       moves = 5;
       resetGame();
-      assert.deepEqual(stacks.a, [4,3,2,1]);
+      assert.deepEqual(stacks.a, [4, 3, 2, 1]);
       assert.deepEqual(stacks.b, []);
       assert.deepEqual(stacks.c, []);
       assert.equal(moves, 0);
