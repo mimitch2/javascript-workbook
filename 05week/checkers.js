7487f3adc509369c51e11228cf3f3c3e6b9f40f1
mimitch2 <mike@melodix.com>
2018-02-25 10:29:05 -0600

    started remove checker method, but now need to break out all the jump checks to return specific scenarios

--- 05week/checkers.js
+++ 05week/checkers.js
@@ -17,11 +17,11 @@ const rl = readline.createInterface({
 6. check for win would evaluate each players count each turn
 7. add reset fucntion to reset all variables and objects to orginial states
 */
-
-const black = new Checker("Black", 'B');//FIXME can I reduce this scope????
-const red = new Checker('Red', 'R');
+const black = new Checker("Black", `\u{26ab}`); //FIXME can I reduce this scope????
+const red = new Checker('Red', `\u{1F534}`);
 let turn = red;
-const move = new Board([null, null], [null, null]);//create Board instance that stores start and end postions
+
+// const move = new Board([null, null], [null, null]);//create Board instance that stores start and end postions
 
 const isInputValid = (whichPiece, toWhere) => { //FIXME mixed good/bad numbers are passing and they should'nt
   if (Number(whichPiece) && Number(toWhere)) { //make sure they are numbers
@@ -44,8 +44,8 @@ function Board(begin, end) {
   this.begin = begin;
   this.end = end;
 
-  this.createGrid = () => {// creates an 8x8 array, filled with null values
-    for (let row = 0; row < 8; row++) {  // loop to create the 8 rows
+  this.createGrid = () => { // creates an 8x8 array, filled with null values
+    for (let row = 0; row < 8; row++) { // loop to create the 8 rows
       this.grid[row] = [];
       for (let column = 0; column < 8; column++) {
         this.grid[row].push(null); // push in 8 columns of nulls
@@ -53,12 +53,12 @@ function Board(begin, end) {
     }
   }
 
-  this.viewGrid = () => {  // prints out the board
+  this.viewGrid = () => { // prints out the board
     // console.log('5 --- inside viewGrid method which prints out the board')
-    let string = "  0 1 2 3 4 5 6 7\n";// add our column numbers
-    for (let row = 0; row < 8; row++) {// we start with our row number in our array
+    let string = "  0 1 2 3 4 5 6 7\n"; // add our column numbers
+    for (let row = 0; row < 8; row++) { // we start with our row number in our array
       const rowOfCheckers = [row];
-      for (let column = 0; column < 8; column++) {  // a loop within a loop
+      for (let column = 0; column < 8; column++) { // a loop within a loop
         // if the location is "truthy" (contains a checker piece, in this case)
         if (this.grid[row][column]) {
           // push the symbol of the check in that location into the array
@@ -69,9 +69,9 @@ function Board(begin, end) {
       }
       // join the rowOfCheckers array to a string, separated by a space
       string += rowOfCheckers.join(' ');
-      string += "\n";// add a 'new line'
+      string += "\n"; // add a 'new line'
     }
-    console.log('Current turn = ', turn.name);//annouce turn each time
+    console.log('Current turn = ', turn.name); //annouce turn each time
     console.log(string);
 
   }
@@ -95,74 +95,14 @@ function Board(begin, end) {
     }
   }
 
-  this.isMoveLegal = () => {
-    // console.log("INSIDE ISLEGAL");
-    if (this.grid[move.begin[0]][move.begin[1]] === turn && //only can move own checker AND
-      this.grid[move.end[0]][move.end[1]] === null) { //only can move to empty spot
-      if (move.end[1] === move.begin[1] + 1 || //and only move +1 column
-        move.end[1] === move.begin[1] - 1) { //OR only move -1 column
-        if (turn === black && move.end[0] === move.begin[0] + 1 || //back can only move +1 row
-          turn === red && move.end[0] === move.begin[0] - 1) { //OR red can only move -1 row
-          return true
-        }
-      } else if (this.canItJump()) { //check for jump move FIXME should this be checked in movepiece?
-        return true
-      }
-    }
-  } //end legal check
 
-  this.canItJump = () => {//FIXME refactor this, it's WAY too long
-    if (turn === black) {
-      if (move.end[0] === move.begin[0] + 2) { //if trying to move +2 rows black
-        if (move.end[1] === move.begin[1] + 2) { //and +2 columns it's a right jump FIXME checking this twice
-          if (this.grid[move.end[0] - 1][move.end[1] - 1] !== turn &&
-            this.grid[move.end[0] - 1][move.end[1] - 1] !== null) { //check that offset piece is not black or null
-            return true
-          }
-        } else if ((move.end[1] === move.begin[1] - 2)) { //or -2 columns it's a left jump FIXME checking this twice
-          if (this.grid[move.end[0] - 1][move.end[1] + 1] !== turn &&
-            this.grid[move.end[0] - 1][move.end[1] + 1] !== null) { //check that offset piece is not black or null
-            return true
-          }
-        }
-      } //end black check
-    } else if (turn === red) {
-      if (move.end[0] === move.begin[0] - 2) { //if trying to move -2 rows red
-        if (move.end[1] === move.begin[1] + 2) { //and +2 columns it's a right jump
-          if (this.grid[move.end[0] + 1][move.end[1] - 1] !== turn &&
-            this.grid[move.end[0] + 1][move.end[1] - 1] !== null) { //check that offset piece is not red or null
-            return true
-          }
-        } else if ((move.end[1] === move.begin[1] - 2)) { //or -2 columns it's a left jump
-          if (this.grid[move.end[0] + 1][move.end[1] + 1] !== turn &&
-            this.grid[move.end[0] + 1][move.end[1] + 1] !== null) { //check that offset piece is not red or null
-            return true
-          }
-        }
-      }
-    } //end red check
-  } //end jump check
-
-  this.moveIt = () => { //FIXME need to get this working INSIDE moveChecker
-    // console.log('-----Inside moveIt');
-    this.grid[move.begin[0]].splice([move.begin[1]], 1, null)
-    this.grid[move.end[0]].splice([move.end[1]], 1, turn)
-    if (turn === red) {
-      turn = black;
-    } else {
-      turn = red;
-    }
-  }
 } //end board class
 
 function Game() {
-  // console.log('1 --- inside Game class');
-  this.board = new Board(); //makes new instance of Board class FIXME Maybe this should be move
-
+  let blackOnBoard = 12; //start with 12 of each to track when one is removed
+  let redOnBoard = 12;
+  this.board = new Board([null, null], [null, null]); //makes new instance of Board class
   this.start = () => {
-    console.log('2----Inside start method');
-    console.log('this = ', game);
-    console.log('move = ', move);
     this.board.createGrid();
     this.board.fillBoard();
   };
@@ -170,11 +110,12 @@ function Game() {
   this.moveChecker = (whichPiece, toWhere) => { //move the checker if legal
     if (isInputValid(whichPiece, toWhere)) {
       this.parsInput(whichPiece, toWhere);
-      if (this.board.isMoveLegal()) { //FIXME put jump check here with condition, then call remove function?
+      if (this.isMoveLegal()) { //FIXME put jump check here with condition, then call remove function?
         console.log("LEGAL MOVE");
 
 
-        this.board.moveIt(); //FIXME need to get this working INSIDE moveChecker
+        this.moveIt(); //FIXME need to get this working INSIDE moveChecker
+
       } else {
         console.log('Illegal Move!!');
       }
@@ -183,19 +124,87 @@ function Game() {
     }
   }
 
-  this.parsInput = (whichPiece, toWhere) => {//split and parse inputs into arays with numbers
+  this.parsInput = (whichPiece, toWhere) => { //split and parse inputs into arays with numbers
     whichPiece = whichPiece.split('');
     toWhere = toWhere.split('');
-    const numberwhichPiece = whichPiece.map((num)=> {
+    const numberwhichPiece = whichPiece.map((num) => {
       return parseInt(num);
     });
-    const numberToWhere = toWhere.map((num2)=> {
+    const numberToWhere = toWhere.map((num2) => {
       return parseInt(num2);
     });
-    move.begin = numberwhichPiece;//make move object's start the new array
-    move.end = numberToWhere;//make move object's end the new array
+    this.board.begin = numberwhichPiece; //make move object's start the new array
+    this.board.end = numberToWhere; //make move object's end the new array
   }
-}
+
+  this.isMoveLegal = () => {
+    // console.log(this);
+    // console.log(this.board.begin);
+    // console.log(this.board.grid);
+    // console.log("INSIDE ISLEGAL");
+    if (this.board.grid[this.board.begin[0]][this.board.begin[1]] === turn && //only can move own checker AND
+      this.board.grid[this.board.end[0]][this.board.end[1]] === null) { //only can move to empty spot
+      if (this.board.end[1] === this.board.begin[1] + 1 || //and only move +1 column
+        this.board.end[1] === this.board.begin[1] - 1) { //OR only move -1 column
+        if (turn === black && this.board.end[0] === this.board.begin[0] + 1 || //back can only move +1 row
+          turn === red && this.board.end[0] === this.board.begin[0] - 1) { //OR red can only move -1 row
+          return true
+        }
+      } else if (this.canItJump()) { //check for jump move FIXME should this be checked in movepiece?
+        return true
+      }
+    }
+  } //end legal check
+
+  this.canItJump = () => { //FIXME refactor this, it's WAY too long
+    if (turn === black) {
+      if (this.board.end[0] === this.board.begin[0] + 2) { //if trying to move +2 rows black
+        if (this.board.end[1] === this.board.begin[1] + 2) { //and +2 columns it's a right jump FIXME checking this twice
+          if (this.board.grid[this.board.end[0] - 1][this.board.end[1] - 1] !== turn &&
+            this.board.grid[this.board.end[0] - 1][this.board.end[1] - 1] !== null) { //check that offset piece is not black or null
+            return true
+          }
+        } else if ((this.board.end[1] === this.board.begin[1] - 2)) { //or -2 columns it's a left jump FIXME checking this twice
+          if (this.board.grid[this.board.end[0] - 1][this.board.end[1] + 1] !== turn &&
+            this.board.grid[this.board.end[0] - 1][this.board.end[1] + 1] !== null) { //check that offset piece is not black or null
+            return true
+          }
+        }
+      } //end black check
+    } else if (turn === red) {
+      if (this.board.end[0] === this.board.begin[0] - 2) { //if trying to move -2 rows red
+        if (this.board.end[1] === this.board.begin[1] + 2) { //and +2 columns it's a right jump
+          if (this.board.grid[this.board.end[0] + 1][this.board.end[1] - 1] !== turn &&
+            this.board.grid[this.board.end[0] + 1][this.board.end[1] - 1] !== null) { //check that offset piece is not red or null
+            return true
+          }
+        } else if ((this.board.end[1] === this.board.begin[1] - 2)) { //or -2 columns it's a left jump
+          if (this.board.grid[this.board.end[0] + 1][this.board.end[1] + 1] !== turn &&
+            this.board.grid[this.board.end[0] + 1][this.board.end[1] + 1] !== null) { //check that offset piece is not red or null
+            return true
+          }
+        }
+      }
+    } //end red check
+  } //end jump check
+  this.moveIt = () => { //FIXME need to get this working INSIDE moveChecker
+    this.board.grid[this.board.begin[0]].splice([this.board.begin[1]], 1, null)
+    this.board.grid[this.board.end[0]].splice([this.board.end[1]], 1, turn)
+    if (turn === red) {
+      turn = black;
+    } else {
+      turn = red;
+    }
+
+  }
+
+  // this.killChecker = () => {
+  //   console.log(this.board.grid);
+  //   this.board.grid.splice(this.board.grid[this.board.end[0] - 1][this.board.end[1] - 1], 1);
+  //   this.killChecker(this.board.grid[this.board.end[0] - 1][this.board.end[1] - 1])
+  // }
+
+} //end Game class
 
 function getPrompt() {
   game.board.viewGrid(); // call to print out initial board
@@ -210,10 +219,6 @@ function getPrompt() {
 const game = new Game(); //creates a new Game class instance
 game.start(); //passed game instance to the start method inside of Game class
 
-
-
-
-
 // Tests
 
 if (typeof describe === 'function') {
@@ -222,7 +227,7 @@ if (typeof describe === 'function') {
       assert.equal(game.board.constructor.name, 'Board');
     });
     it('board should have 24 checkers', () => {
-      assert.equal(game.board.checkers.length, 24);//FIXME WTF is checkers?????
+      assert.equal(game.board.checkers.length, 24); //FIXME WTF is checkers????? I bet it's tracking how many checks on the board
     });
   });
 
@@ -240,7 +245,7 @@ if (typeof describe === 'function') {
       game.moveChecker('30', '52');
       assert(game.board.grid[5][2]);
       assert(!game.board.grid[4][1]);
-      assert.equal(game.board.checkers.length, 23);//FIXME WTF is checkers?????
+      assert.equal(game.board.checkers.length, 23); //FIXME WTF is checkers?????
     });
   });
 } else {
