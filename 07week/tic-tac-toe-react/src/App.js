9f7d868219d18bf1664693a5908065ac5bd8e56a
mimitch2 <mike@melodix.com>
2018-03-02 13:45:24 -0600

    got the board to render whatever player turn it was when any cell is clikced

--- 07week/tic-tac-toe-react/src/App.js
+++ 07week/tic-tac-toe-react/src/App.js
@@ -3,23 +3,49 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      isCellEmpty: true,
+      player: 'X',
+      board: [
+         [[null], [null], [null]],
+         [[null], [null], [null]],
+         [[null], [null], [null]]
+       ]
+
+    }
+  }
+
+
+
+  handleClickCell=(event)=>{
+    const wasClicked = event.target.getAttribute("data-cell").split('').map((num)=>{
+          return Number(num);//get the data-cell value for whatever was clicked, and turn into array of numbers
+    });
+    const temp = this.state.board//set a temporary container to hold this.state.board
+    temp[wasClicked[0]][wasClicked[1]]=this.state.player//assign the array index to current player
+      this.setState({temp: temp});
+  }
+
+
   render() {
     return (
       <div>
         <div className="row">
-          <div data-cell="0">X</div>
-          <div data-cell="1"></div>
-          <div data-cell="2"></div>
+          <div data-cell="00" onClick={this.handleClickCell}>{this.state.board[0][0]}</div>
+          <div data-cell="01" onClick={this.handleClickCell}>{this.state.board[0][1]}</div>
+          <div data-cell="02" onClick={this.handleClickCell}>{this.state.board[0][2]}</div>
         </div>
         <div className="row">
-          <div data-cell="3"></div>
-          <div data-cell="4"></div>
-          <div data-cell="5"></div>
+          <div data-cell="10" onClick={this.handleClickCell}>{this.state.board[1][0]}</div>
+          <div data-cell="11" onClick={this.handleClickCell}>{this.state.board[1][1]}</div>
+          <div data-cell="12" onClick={this.handleClickCell}>{this.state.board[1][2]}</div>
         </div>
         <div className="row">
-          <div data-cell="6"></div>
-          <div data-cell="7"></div>
-          <div data-cell="8"></div>
+          <div data-cell="20" onClick={this.handleClickCell}>{this.state.board[2][0]}</div>
+          <div data-cell="21" onClick={this.handleClickCell}>{this.state.board[2][1]}</div>
+          <div data-cell="22" onClick={this.handleClickCell}>{this.state.board[2][2]}</div>
         </div>
       </div>
     );
